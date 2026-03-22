a97b3a0589a0d586e5a529a89e713fadf9624877
Heng-I Chu <chu244@asicfab.ecn.purdue.edu>
2026-03-22 01:29:42 -0400

    axi bus interface update

--- src/axi/axi_bus_if.sv
+++ src/axi/axi_bus_if.sv
@@ -8,33 +8,43 @@ interface axi_bus_if(input logic CLK, input logic nRST);
 
     import axi_bus_pkg::*;
 
-    // master side channel structs
-    master_ar_channel_t ar_sp0_i;
-    master_ar_channel_t ar_sp1_i;
-    master_ar_channel_t ar_d_i;
-    master_ar_channel_t ar_i_i;
-    master_r_channel_t  r_sp0_o;
-    master_r_channel_t  r_sp1_o;
-    master_r_channel_t  r_d_o;
-    master_r_channel_t  r_i_o;
-    master_aw_channel_t aw_sp0_i;
-    master_aw_channel_t aw_sp1_i;
-    master_aw_channel_t aw_d_i;
-    master_w_channel_t  w_sp0_i;
-    master_w_channel_t  w_sp1_i;
-    master_w_channel_t  w_d_i;
-    master_b_channel_t  b_sp0_o;
-    master_b_channel_t  b_sp1_o;
-    master_b_channel_t  b_d_o;
+    // master side channel structs 
+    master_ar_channel_t ar_sp0_i; // Scratchpad0 AR channel
+    master_ar_channel_t ar_sp1_i; // Scratchpad1 AR channel
+    master_ar_channel_t ar_d_i;   // D$ AR channel
+    master_ar_channel_t ar_i_i;   // I$ AR channel
+    master_r_channel_t  r_sp0_o;  // Scratchpad0 R channel
+    master_r_channel_t  r_sp1_o;  // Scratchpad1 R channel
+    master_r_channel_t  r_d_o;    // D$ R channel
+    master_r_channel_t  r_i_o;    // I$ R channel
+    master_aw_channel_t aw_sp0_i; // Scratchpad0 AW channel
+    master_aw_channel_t aw_sp1_i; // Scratchpad1 AW channel
+    master_aw_channel_t aw_d_i;   // D$ AW channel
+    master_w_channel_t  w_sp0_i;  // Scratchpad0 W channel
+    master_w_channel_t  w_sp1_i;  // Scratchpad1 W channel
+    master_w_channel_t  w_d_i;    // D$ W channel
+    master_b_channel_t  b_sp0_o;  // Scratchpad0 B channel
+    master_b_channel_t  b_sp1_o;  // Scratchpad1 B channel
+    master_b_channel_t  b_d_o;    // D$ B channel
+
+    // general aw channel struct (master side)
+    master_aw_channel_t aw_gen_i; // General AW Channel for Write Manager
+    // general w channel struct (master side)
+    master_w_channel_t w_gen_i; // General W Channel for Write Manager
 
     // subordinate side channel structs
-    sub_ar_channel_t ar_o;
-    sub_r_channel_t  r_i;
-    sub_aw_channel_t aw_o;
-    sub_w_channel_t  w_o;
-    sub_b_channel_t  b_i;
+    sub_ar_channel_t ar_o;     // Controller AR channel
+    sub_r_channel_t  r_i;      // Controller R channel
+    sub_aw_channel_t aw_o;     // Controller AW channel
+    sub_w_channel_t  w_o;      // Controller W channel
+    sub_b_channel_t  b_i;      // Controller B channel
 
-    // read arbiter signals
+    // general aw channel struct (subordinate side)
+    sub_aw_channel_t head_aw_o; // General AW Channel for Write Manager
+    // general w channel struct (subordinate side)
+    sub_w_channel_t head_w_o; // General W Channel for Write Manager
+
+    // read arbiter signals 
     logic sp0_req_r, sp1_req_r, d_req_r, i_req_r, skid_ready_r;
     logic [ARGRANT-1:0] ar_grant;
 
@@ -43,39 +53,328 @@ interface axi_bus_if(input logic CLK, input logic nRST);
     logic [AWLEN-1:0] sp0_len_w, sp1_len_w, d_len_w;
     logic [AWGRANT-1:0] aw_grant;
 
+    // SP0 & AR MANAGER READY/VALID
     logic ar_sp0_i_valid, ar_sp0_i_ready;
+
+    // SP1 & AR MANAGER READY/VALID
     logic ar_sp1_i_valid, ar_sp1_i_ready;
-    logic ar_d_i_valid,   ar_d_i_ready;
-    logic ar_i_i_valid,   ar_i_i_ready;
-    logic ar_o_valid,     ar_o_ready;
-    logic r_i_valid,      r_i_ready;
-    logic r_sp0_o_valid,  r_sp0_o_ready;
-    logic r_sp1_o_valid,  r_sp1_o_ready;
-    logic r_d_o_valid,    r_d_o_ready;
-    logic r_i_o_valid,    r_i_o_ready;
+
+    // D$ & AR MANAGER READY/VALID
+    logic ar_d_i_valid, ar_d_i_ready;
+
+    // I$ & AR MANAGER READY/VALID
+    logic ar_i_i_valid, ar_i_i_ready;
+
+    // DRAM CONTROLLER & READ SKID BUFFER READY/VALID
+    logic ar_o_valid, ar_o_ready;
+
+    // DRAM CONTROLLER & READ RESPONSE ROUTER READY/VALID
+    logic r_i_valid, r_i_ready;
+
+    // SP0 R Skid Buffer && SP0 READY/VALID
+    logic r_sp0_o_valid, r_sp0_o_ready;
+
+    // SP1 R Skid Buffer && SP1 READY/VALID
+    logic r_sp1_o_valid, r_sp1_o_ready;
+
+    // D$ R Skid Buffer && D$ READY/VALID
+    logic r_d_o_valid, r_d_o_ready;
+
+    // I$ R Skid Buffer && I$ READY/VALID
+    logic r_i_o_valid, r_i_o_ready;
+
+    // SP0 & AW_W MANAGER READY/VALID
     logic aw_sp0_i_valid, aw_sp0_i_ready, w_sp0_i_valid, w_sp0_i_ready;
+
+    // SP1 & AW_W MANAGER READY/VALID
     logic aw_sp1_i_valid, aw_sp1_i_ready, w_sp1_i_valid, w_sp1_i_ready;
-    logic aw_d_i_valid,   aw_d_i_ready,   w_d_i_valid,   w_d_i_ready;
-    logic aw_o_valid,     aw_o_ready,     w_o_valid,     w_o_ready;
-    logic b_i_valid,      b_i_ready;
-    logic b_sp0_o_valid,  b_sp0_o_ready;
-    logic b_sp1_o_valid,  b_sp1_o_ready;
-    logic b_d_o_valid,    b_d_o_ready;
 
-    modport ar_to_subordinate   (output ar_o_valid, ar_o,   input  ar_o_ready);
-    modport subordinate_to_r    (output r_i_ready,          input  r_i_valid, r_i);
-    modport aw_to_subordinate   (output aw_o_valid, aw_o,  input  aw_o_ready);
-    modport w_to_subordinate    (output w_o_valid,  w_o,   input  w_o_ready);
-    modport subordinate_to_b    (output b_i_ready,          input  b_i_valid, b_i);
+    // D$ & AW_W MANAGER READY/VALID
+    logic aw_d_i_valid, aw_d_i_ready, w_d_i_valid, w_d_i_ready;
 
-    modport read_arbiter  (input  sp0_req_r, sp1_req_r, d_req_r, i_req_r, skid_ready_r,
-                           output ar_grant);
-    modport write_arbiter (input  sp0_req_w, sp1_req_w, d_req_w,
-                           input  sp0_len_w, sp1_len_w, d_len_w, skid_ready_w,
-                           output aw_grant);
-    modport write_arbiter_tb (output sp0_req_w, sp1_req_w, d_req_w,
-                              output sp0_len_w, sp1_len_w, d_len_w, skid_ready_w,
-                              input  aw_grant);
+    // DRAM CONTROLLER & WRITE SKID BUFFER READY/VALID
+    logic aw_o_valid, aw_o_ready, w_o_valid, w_o_ready;
+
+    // DRAM CONTROLLER & WRITE RESPONSE ROUTER
+    logic b_i_valid, b_i_ready;
+
+    // SP0 B Skid Buffer && SP0 READY/VALID
+    logic b_sp0_o_valid, b_sp0_o_ready;
+
+    // SP1 B Skid Buffer && SP1 READY/VALID
+    logic b_sp1_o_valid, b_sp1_o_ready;
+
+    // D$ R Skid Buffer && D$ READY/VALID
+    logic b_d_o_valid, b_d_o_ready;
+
+    // WRITE MANAGER SIGNALS
+    logic awvalid, awready; // GENERAL AW WRITE MANAGER READY/VALID
+    logic wvalid, wready;   // GENERAL W WRITE MANAGER READY/VALID
+    logic aw_pop, w_pop; 
+    logic head_awvalid, head_wvalid;
+
+    // test assertions
+    property wrt_valid_ready;
+        @(posedge CLK)
+        (awvalid && !awready) |-> $stable(aw_gen_i);
+    endproperty
+
+    assert property (wrt_valid_ready)
+        else $error("data changed during low ready");
+
+    // ----------------------------------------------------------------------
+    // READ PATH Definitions
+    // ----------------------------------------------------------------------
+    // MASTER <=> SP0 AR MANAGER
+    modport ar_sp0_manager (
+        // From Master 
+        input ar_sp0_i_valid, ar_sp0_i,
+
+        // To Master 
+        output ar_sp0_i_ready
+    );
+
+    // MASTER <=> SP1 AR MANAGER
+    modport ar_sp1_manager (
+        // From Master 
+        input ar_sp1_i_valid, ar_sp1_i,
+
+        // To Master 
+        output ar_sp1_i_ready
+    );
+
+    // MASTER <=> D$ AR MANAGER
+    modport ar_d_manager (
+        // From Master 
+        input ar_d_i_valid, ar_d_i,
+
+        // To Master 
+        output ar_d_i_ready
+    );
+
+    // MASTER <=> I$ AR MANAGER
+    modport ar_i_manager (
+        // From Master 
+        input ar_i_i_valid, ar_i_i,
+
+        // To Master 
+        output ar_i_i_ready
+    );
+
+    // DRAM CONTROLLER <=> READ SKID BUFFER
+    modport ar_to_subordinate (
+        // To Subordinate
+        output ar_o_valid, ar_o,
+
+        // From Subordinate
+        input ar_o_ready
+    );
+
+    // DRAM CONTROLLER <=> READ RESPONSE ROUTER
+    modport subordinate_to_r (
+        // To Subordinate
+        output r_i_ready, 
+
+        // From Subordinate
+        input r_i_valid, r_i
+    );
+
+    // SP0 R Skid Buffer <=> SP0
+    modport r_to_sp0 (
+        // To Master 
+        output r_sp0_o_valid, r_sp0_o,
+
+        // From Master
+        input r_sp0_o_ready
+    );
+
+    // SP1 R Skid Buffer <=> SP1
+    modport r_to_sp1 (
+        // To Master 
+        output r_sp1_o_valid, r_sp1_o,
+
+        // From Master
+        input r_sp1_o_ready
+    );
+
+    // D$ R Skid Buffer <=> D$
+    modport r_to_d (
+        // To Master 
+        output r_d_o_valid, r_d_o,
+
+        // From Master
+        input r_d_o_ready
+    );
+
+    // I$ R Skid Buffer <=> I$
+    modport r_to_i (
+        // To Master 
+        output r_i_o_valid, r_i_o,
+
+        // From Master
+        input r_i_o_ready
+    );
+
+    // AR MANAGERS <=> READ ARBITER
+    modport read_arbiter (
+        // From Manager
+        input sp0_req_r, sp1_req_r, d_req_r, i_req_r,
+
+        // From Skid Buffer
+        input skid_ready_r,
+
+        // To Read Mux
+        output ar_grant
+    );
+
+    // ----------------------------------------------------------------------
+    // WRITE PATH Definitions
+    // ----------------------------------------------------------------------
+    // MASTER <=> SP0 AW_W MANAGER
+    modport aw_w_sp0_manager (
+        // From Master 
+        input aw_sp0_i_valid, aw_sp0_i, w_sp0_i_valid, w_sp0_i,
+
+        // To Master 
+        output aw_sp0_i_ready, w_sp0_i_ready
+    );
+
+    // MASTER <=> SP1 AW_W MANAGER
+    modport aw_w_sp1_manager (
+        // From Master 
+        input aw_sp1_i_valid, aw_sp1_i, w_sp1_i_valid, w_sp1_i,
+
+        // To Master 
+        output aw_sp1_i_ready, w_sp1_i_ready
+    );
+
+    // MASTER <=> D$ AW_W MANAGER
+    modport aw_w_d_manager (
+        // From Master 
+        input aw_d_i_valid, aw_d_i, w_d_i_valid, w_d_i,
+
+        // To Master 
+        output aw_d_i_ready, w_d_i_ready
+    );
+
+    // DRAM CONTROLLER <=> WRITE SKID BUFFER
+    modport aw_to_subordinate (
+        // To Subordinate
+        output aw_o_valid, aw_o,
+
+        // From Subordinate
+        input aw_o_ready
+    );
+
+    // DRAM CONTROLLER <=> WRITE SKID BUFFER
+    modport w_to_subordinate (
+        // To Subordinate
+        output w_o_valid, w_o,
+
+        // From Subordinate
+        input w_o_ready
+    );
+
+    // DRAM CONTROLLER <=> WRITE RESPONSE ROUTER
+    modport subordinate_to_b (
+        // To Subordinate
+        output b_i_ready,
+
+        // From Subordinate
+        input b_i_valid, b_i
+    );
+
+    // SP0 B Skid Buffer <=> SP0
+    modport b_to_sp0 (
+        // To Master 
+        output b_sp0_o_valid, b_sp0_o,
+
+        // From Master
+        input b_sp0_o_ready
+    );
+
+    // SP1 B Skid Buffer <=> SP1
+    modport b_to_sp1 (
+        // To Master 
+        output b_sp1_o_valid, b_sp1_o,
+
+        // From Master
+        input b_sp1_o_ready
+    );
+
+    // D$ B Skid Buffer <=> D$
+    modport b_to_d (
+        // To Master 
+        output b_d_o_valid, b_d_o,
+
+        // From Master
+        input b_d_o_ready
+    );
+
+    // WRITE ARBITER
+    modport write_arbiter (
+        // From Manager
+        input sp0_req_w, sp1_req_w, d_req_w,
+        input sp0_len_w, sp1_len_w, d_len_w,
+
+        // From Skid Buffer
+        input skid_ready_w,
+
+        // To Read Mux/AR Manager 
+        output aw_grant
+    );
+
+    // WRITE MANAGER
+    modport write_manager (
+        // From Master AW Channel
+        input awvalid, aw_gen_i,
+        // To Master AW Channel
+        output awready,
+        // From Master W Channel
+        input wvalid, w_gen_i,
+        // To Master W Channel
+        output wready, 
+        // From Write Controller
+        input aw_pop, w_pop,
+        // To AW MUX
+        output head_awvalid, head_aw_o,
+        // To W MUX
+        output head_wvalid, head_w_o
+    );
+
+    // ----------------------------------------------------------------------
+    // WRITE PATH TB Definitions
+    // ----------------------------------------------------------------------
+    // WRITE ARBITER TB
+    modport write_arbiter_tb (
+        // From Manager
+        output sp0_req_w, sp1_req_w, d_req_w,
+        output sp0_len_w, sp1_len_w, d_len_w,
+
+        // From Skid Buffer
+        output skid_ready_w,
+
+        // To Read Mux/AR Manager 
+        input aw_grant
+    );
+
+    // WRITE MANAGER TB
+    modport write_manager_tb (
+        // From Master AW Channel
+        output awvalid, aw_gen_i,
+        // To Master AW Channel
+        input awready,
+        // From Master W Channel
+        output wvalid, w_gen_i,
+        // To Master W Channel
+        input wready, 
+        // From Write Controller
+        output aw_pop, w_pop,
+        // To AW MUX
+        input head_awvalid, head_aw_o,
+        // To W MUX
+        input head_wvalid, head_w_o 
+    );
 
 endinterface
 `endif // AXI_BUS_IF_SV
--- src/axi/axi_bus_pkg.sv
+++ src/axi/axi_bus_pkg.sv
@@ -6,6 +6,10 @@
 package axi_bus_pkg;
     `include "axi_bus_params.svh"
 
+    //////////////////////////////////////////////////////////////////////
+    ///////////////////////// Derived Parameters /////////////////////////
+    //////////////////////////////////////////////////////////////////////
+    
     // Master ID Width
     localparam int MID            = $clog2(NUM_MASTERS);
 
@@ -13,7 +17,7 @@ package axi_bus_pkg;
     localparam int ARADDR         = ADDR_WIDTH;
     localparam int ARID           = $clog2(NUM_U_READS);
     localparam int ARSIZE         = 3;
-    localparam int ARLEN          = 4;
+    localparam int ARLEN          = 4; // describes how many beats in a transfer. 
     localparam int ARBURST        = 2;
     localparam int MID_ARID       = MID + ARID;
 
@@ -27,14 +31,14 @@ package axi_bus_pkg;
     localparam int AWADDR         = ADDR_WIDTH;
     localparam int AWID           = $clog2(NUM_U_WRITES);
     localparam int AWSIZE         = 3;
-    localparam int AWLEN          = 4;
+    localparam int AWLEN          = 4; // describes how many beats in a transfer.
     localparam int AWBURST        = 2;
     localparam int MID_AWID       = MID + AWID;
 
     // W Channel Width
     localparam int WDATA          = DATA_BEAT_WIDTH;
     localparam int WID            = $clog2(NUM_U_WRITES);
-    localparam int WSTRB          = DATA_BEAT_WIDTH/8;
+    localparam int WSTRB          = DATA_BEAT_WIDTH/8;    // 1 bit for each byte in data beat
     localparam int MID_WID        = MID + WID;
 
     // B Channel Width
@@ -42,50 +46,62 @@ package axi_bus_pkg;
     localparam int BRESP          = 2;
     localparam int MID_BID        = MID + BID;
 
-    // Arbiter widths
-    localparam int ARGRANT        = 1 + ARID;
-    localparam int AWGRANT        = 1 + AWID;
+    // READ Arbiter Width
+    localparam int ARGRANT        = 1 + ARID; // 1 valid bit and then bits representing which master
 
-    // FIFO depths
+    // WRITE Arbiter Width
+    localparam int AWGRANT        = 1 + AWID; // 1 valid bit and then bits representing which master
+
+    // AR Manager FIFO Width
     localparam int AR_DEPTH       = NUM_U_READS;
     localparam int AR_PTR_WIDTH   = $clog2(AR_DEPTH);
+
+    // AW+W Manager AW FIFO Width
     localparam int AW_DEPTH       = NUM_U_WRITES;
-    localparam int AW_PTR_WIDTH   = $clog2(AW_DEPTH);
+    localparam int AW_PTR_WIDTH   = $clog2(AW_DEPTH); 
+
+    // AW+W Manager W FIFO Width
     localparam int W_DEPTH        = NUM_U_WRITES*8;
-    localparam int W_PTR_WIDTH    = $clog2(W_DEPTH);
+    localparam int W_PTR_WIDTH   = $clog2(W_DEPTH); 
+
+    //////////////////////////////////////////////////////////////////////
+    ////////////////////////////////// Enums /////////////////////////////
+    //////////////////////////////////////////////////////////////////////
 
     // Master IDs
     typedef enum logic [MID-1:0] {
-        SP0    = 2'b00,
-        SP1    = 2'b01,
-        DCACHE = 2'b10,
-        ICAHCE = 2'b11
+        SP0              = 2'b00,
+        SP1              = 2'b01,
+        DCACHE           = 2'b10,
+        ICAHCE           = 2'b11
     } mid_t;
 
     // RRESP options
     typedef enum logic [RRESP-1:0] {
-        R_OKAY   = 2'b00,
-        R_EXOKAY = 2'b01,
-        R_SLVERR = 2'b10,
-        R_DECERR = 2'b11
+        R_OKAY           = 2'b00,
+        R_EXOKAY         = 2'b01,
+        R_SLVERR         = 2'b10, 
+        R_DECERR         = 2'b11
     } rresp_t;
 
     // BRESP options
     typedef enum logic [BRESP-1:0] {
-        B_OKAY   = 2'b00,
-        B_EXOKAY = 2'b01,
-        B_SLVERR = 2'b10,
-        B_DECERR = 2'b11
+        B_OKAY           = 2'b00,
+        B_EXOKAY         = 2'b01,
+        B_SLVERR         = 2'b10,
+        B_DECERR         = 2'b11
     } bresp_t;
 
+    // AR Channel From Master 
     typedef struct packed {
         logic [ARADDR-1:0]   addr;
-        logic [ARID-1:0]     id;
+        logic [ARID-1:0]     id;     // Local (2-bit) ID: index within master
         logic [ARSIZE-1:0]   size;
         logic [ARLEN-1:0]    len;
         logic [ARBURST-1:0]  burst;
     } master_ar_channel_t;
 
+    // R Channel To Master 
     typedef struct packed {
         logic [RDATA-1:0]   data;
         logic [RID-1:0]     id;
@@ -93,6 +109,7 @@ package axi_bus_pkg;
         rresp_t             resp;
     } master_r_channel_t;
 
+    // AW Channel From Master 
     typedef struct packed {
         logic [AWADDR-1:0]   addr;
         logic [AWID-1:0]     id;
@@ -101,6 +118,7 @@ package axi_bus_pkg;
         logic [AWBURST-1:0]  burst;
     } master_aw_channel_t;
 
+    // W Channel From Master 
     typedef struct packed {
         logic [WDATA-1:0]   data;
         logic [WID-1:0]     id;
@@ -108,19 +126,22 @@ package axi_bus_pkg;
         logic [WSTRB-1:0]   strb;
     } master_w_channel_t;
 
+    // B Channel
     typedef struct packed {
         logic [BID-1:0]     id;
         bresp_t             resp;
     } master_b_channel_t;
 
+    // AR Channel To Subordiante
     typedef struct packed {
         logic [ARADDR-1:0]   addr;
-        logic [MID_ARID-1:0] mid_id;
+        logic [MID_ARID-1:0] mid_id; // Global (4-bit) ID: {MASTER_ID, id}
         logic [ARSIZE-1:0]   size;
         logic [ARLEN-1:0]    len;
         logic [ARBURST-1:0]  burst;
     } sub_ar_channel_t;
 
+    // R Channel From Subordinate
     typedef struct packed {
         logic [RDATA-1:0]   data;
         logic [RID-1:0]     id;
@@ -128,35 +149,41 @@ package axi_bus_pkg;
         rresp_t             resp;
     } sub_r_channel_t;
 
+    // AW Channel To Subordinate
     typedef struct packed {
         logic [AWADDR-1:0]   addr;
-        logic [MID_AWID-1:0] mid_id;
+        logic [MID_AWID-1:0] mid_id; // Global (4-bit) ID: {MASTER_ID, id}
         logic [AWSIZE-1:0]   size;
         logic [AWLEN-1:0]    len;
         logic [AWBURST-1:0]  burst;
     } sub_aw_channel_t;
 
+    // W Channel To Subordiante
     typedef struct packed {
-        logic [MID_ARID-1:0] mid_id;
+        logic [MID_ARID-1:0] mid_id; // Global (4-bit) ID: {MASTER_ID, id}
         logic [WDATA-1:0]    data;
         logic                last;
         logic [WSTRB-1:0]    strb;
     } sub_w_channel_t;
 
+    // B Channel From Subordinate
     typedef struct packed {
         logic [BID-1:0]     id;
         bresp_t             resp;
     } sub_b_channel_t;
 
-    typedef struct packed {
+    // Created structs for all FIFOs/buffer used in the design. Could have reused the channel structs but wanted to seperate the two for better readibility. 
+    // AR MANAGER FIFO payload
+     typedef struct packed {
         logic                valid;
         logic [ARADDR-1:0]   addr;
-        logic [MID_ARID-1:0] mid_id;
+        logic [MID_ARID-1:0] mid_id; // Global (4-bit) ID: {MASTER_ID, id}
         logic [ARSIZE-1:0]   size;
         logic [ARLEN-1:0]    len;
         logic [ARBURST-1:0]  burst;
-    } ar_payload_t;
+     } ar_payload_t;
 
+     // READ RESPONSE SKID BUFFER payload 
     typedef struct packed {
         logic               valid;
         logic [RDATA-1:0]   data;
@@ -165,28 +192,34 @@ package axi_bus_pkg;
         rresp_t             resp;
     } r_payload_t;
 
+    // AW_W MANAGER AW FIFO payload
     typedef struct packed {
         logic                valid;
         logic [AWADDR-1:0]   addr;
-        logic [MID_AWID-1:0] mid_id;
+        logic [MID_AWID-1:0] mid_id; // Global (4-bit) ID: {MASTER_ID, id}
         logic [AWSIZE-1:0]   size;
         logic [AWLEN-1:0]    len;
         logic [AWBURST-1:0]  burst;
     } aw_payload_t;
 
+    // AW_W MANAGER W FIFO payload
     typedef struct packed {
         logic                valid;
-        logic [MID_ARID-1:0] mid_id;
+        logic [MID_ARID-1:0] mid_id; // Global (4-bit) ID: {MASTER_ID, id}
         logic [WDATA-1:0]    data;
         logic                last;
         logic [WSTRB-1:0]    strb;
     } w_payload_t;
 
+    // WRITE RESPONSE SKID BUFFER payload
     typedef struct packed {
         logic               valid;
         logic [BID-1:0]     id;
         bresp_t             resp;
     } b_payload_t;
+
+    // created basic 
+
 
 endpackage
 `endif // AXI_BUS_PKG_SV
